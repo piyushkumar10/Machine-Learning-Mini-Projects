77c87fcffec3b5ba63ade1d83e9ad32643f48ff6
Piyush Kumar <piyush.kumarmaloo@gmail.com>
2020-04-29 00:48:43 +0530

    Add files via upload

---
+++ Classification/Social Media Ads Analyser(KSVM)/SocialNetworksAd Classifier.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+ 
+dataset=pd.read_csv('Social_Network_Ads.csv')
+x=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,4].values
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.25,random_state=0)
+scaler=StandardScaler()
+xtrain=scaler.fit_transform(xtrain)
+xtest=scaler.fit_transform(xtest)
+
+from sklearn.svm import SVC
+classifier=SVC(kernel='rbf',random_state=0)
+classifier.fit(xtrain,ytrain)
+
+classifiedvalue=classifier.predict(xtest)
+
+from sklearn.metrics import confusion_matrix
+confusionmatrix=confusion_matrix(ytest,classifiedvalue)
+from matplotlib.colors import ListedColormap
+
+#test Set Visualisation
+xset,yset=xtest,ytest
+xaxis,yaxis=np.meshgrid(np.arange(start=xset[:,0].min()-1,stop=xset[:,0].max()+1,step=0.01),np.arange(start=xset[:,1].min()-1,stop=xset[:,1].max()+1,step=0.01))
+
+plt.contourf(xaxis,yaxis,classifier.predict(np.array([xaxis.ravel(),yaxis.ravel()]).T).reshape(xaxis.shape),alpha=0.4,cmap=ListedColormap(('red','green')))
+
+plt.xlim(xaxis.min(),xaxis.max())
+plt.ylim(yaxis.min(),yaxis.max())
+for i,j in enumerate(np.unique(yset)):
+    plt.scatter(xset[yset==j,0],xset[yset==j,1],c=ListedColormap(('red','green'))(i),label=j)
+plt.title("SVM(Test)")
+plt.xlabel("Age")
+plt.ylabel("Salary")
+plt.legend()
+plt.show()
---
+++ Clustering/Mall Customers (Hierarchical)/Mall_Customers.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Mall_Customers.csv')  # import datasets
+x = dataset.iloc[:, [3, 4]].values
+
+import scipy.cluster.hierarchy as sch
+
+dendrogram = sch.dendrogram(sch.linkage(x, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distances')
+plt.show()
+
+# Clustering
+
+from sklearn.cluster import AgglomerativeClustering
+
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+clustered = hc.fit_predict(x)
+
+# Plotting 2d in graphs
+
+plt.scatter(x[clustered == 0, 0], x[clustered == 0, 1], s=50, c='red', label="Cluster 1")
+plt.scatter(x[clustered == 1, 0], x[clustered == 1, 1], s=50, c='green', label="Cluster 2")
+plt.scatter(x[clustered == 2, 0], x[clustered == 2, 1], s=50, c='cyan', label="Cluster 3")
+plt.scatter(x[clustered == 3, 0], x[clustered == 3, 1], s=50, c='magenta', label="Cluster 4")
+plt.scatter(x[clustered == 4, 0], x[clustered == 4, 1], s=50, c='blue', label="Cluster 5")
+
+plt.title("Clusters")
+plt.xlabel('Annual Income')
+plt.ylabel("Spending")
+plt.legend()
+plt.show()
